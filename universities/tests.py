67bdd726e0bad7346ada0230b47107a66cec7121
Syed Alif Hasan <sdah24@gmail.com>
2026-04-13 00:52:43 +0600

    SPRINT 2: SCRUM 14 Unit testing SUCCESSFUL :DDD

--- universities/tests.py
+++ universities/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import University
 
-# Create your tests here.
+
+class UniversityTest(TestCase):
+
+    def test_university_creation(self):
+
+        uni = University.objects.create(
+            name="MIT",
+            country="USA",
+            tuition_range="$50000",
+            intake="Fall",
+            description="Top engineering university"
+        )
+
+        self.assertEqual(
+            uni.name,
+            "MIT"
+        )
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from .models import User
+from django.urls import reverse
+
+class UserModelTest(TestCase):
+
+    def test_user_creation(self):
+
+        user = User.objects.create(
+            name="Test User",
+            email="testuser@gmail.com",
+            password="123456"
+        )
+
+        self.assertEqual(
+            user.name,
+            "Test User"
+        )
+
+        self.assertEqual(
+            user.email,
+            "testuser@gmail.com"
+        )
+
+
+
+
+class LoginTest(TestCase):
+
+    def test_login_valid_user(self):
+
+        User.objects.create(
+            name="Login User",
+            email="login@gmail.com",
+            password="123456"
+        )
+
+        response = self.client.post(
+            reverse("login"),
+            {
+                "email": "login@gmail.com",
+                "password": "123456"
+            }
+        )
+
+        self.assertEqual(
+            response.status_code,
+            302
+        )
