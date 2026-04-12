3114865568d254aae71638a3210be84c91c92bc5
Syed Alif Hasan <sdah24@gmail.com>
2026-04-13 00:41:30 +0600

    SPRINT 2 SCRUM 75 done! need some sytlings and refinement

--- universities/views.py
+++ universities/views.py
@@ -8,6 +8,21 @@ def universities_view(request):
 
         universities =  University.objects.all()
 
+        search_query = request.GET.get("search")
+        country_query = request.GET.get("country")
+
+        # Search by name
+        if search_query:
+            universities = universities.filter(
+                name__icontains=search_query
+            )
+
+        # Search by country
+        if country_query:
+            universities = universities.filter(
+                country__icontains=country_query
+            )
+
         return render(request, "universities/universities.html",{"universities": universities}
                       )
 
