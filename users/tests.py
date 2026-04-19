1b96a35e3956bdb4a80cbb78d914ceccbb30c948
Syed Alif Hasan <sdah24@gmail.com>
2026-04-19 23:55:49 +0600

    profile unit test done

--- users/tests.py
+++ users/tests.py
@@ -48,3 +48,52 @@ class LoginTest(TestCase):
             response.status_code,
             302
         )
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class ProfileTestCase(TestCase):
+
+    def setUp(self):
+
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="password123"
+        )
+
+        self.client.login(
+            username="testuser",
+            password="password123"
+        )
+
+    def test_profile_form_loads(self):
+
+        response = self.client.get(
+            reverse("users:create_profile")
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_submission(self):
+
+        response = self.client.post(
+
+            reverse("users:create_profile"),
+
+            {
+
+                "full_name": "Test User",
+
+                "phone_number": "123456789",
+
+                "date_of_birth": "2000-01-01",
+
+                "address": "Dhaka",
+
+            }
+
+        )
+
+        self.assertEqual(response.status_code, 302)
