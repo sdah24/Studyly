eea1526ee40b09a70dab8d6ce36bb734cc9bdbdb
Syed Alif Hasan <sdah24@gmail.com>
2026-04-19 23:54:35 +0600

    profile form, registration done

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+# users/admin.py
+
+from django.contrib import admin
+
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "user",
+        "full_name",
+        "phone_number",
+        "created_at",
+    ]
