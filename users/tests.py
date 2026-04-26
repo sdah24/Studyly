b1842e462a76374bda8af722cabfae55e9136d0a
Syed Alif Hasan <sdah24@gmail.com>
2026-04-26 23:34:41 +0600

    UserM UT TC1

--- users/tests.py
+++ users/tests.py
@@ -11,3 +11,14 @@ class UserRegistrationTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.register_url = reverse('users:register')
+
+    def test_register_with_valid_data(self):
+
+        response = self.client.post(self.register_url, {
+        'username': 'testuser',
+        'email': 'test@example.com',
+         'password1': 'StrongPass@123',
+                'password2': 'StrongPass@123',
+         })
+        self.assertEqual(User.objects.filter(username='testuser').count(), 1)
+        self.assertRedirects(response, reverse('users:login'))
