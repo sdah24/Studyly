359c9e57073e05920f2670bf59e017cfa5bb88d5
Ragib Hasin muktadir <priontomuktadir9@gmail.com>
2026-05-09 09:39:55 +0600

    Add M5 Applications unit tests

--- applications/tests.py
+++ applications/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from universities.models import University
+from applications.models import Application
+import datetime
 
-# Create your tests here.
+User = get_user_model()
+
+
+def make_user(username='appuser', password='pass@123'):
+    return User.objects.create_user(username=username, password=password, role='student')
+
+
+def make_university():
+    return University.objects.create(
+        name='Test University', country='USA', city='Boston',
+        ranking=10, min_gpa=3.0, min_ielts=6.5
+    )
+
+
+class ApplicationCRUDTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = make_user()
+        self.university = make_university()
+        self.list_url = reverse('applications:list')
+        self.create_url = reverse('applications:create')
+
+    def test_create_application_valid(self):
+        self.client.login(username='appuser', password='pass@123')
+        count_before = Application.objects.count()
+        self.client.post(self.create_url, {
+            'university': self.university.pk,
+            'status': 'incomplete',
+            'deadline': (datetime.date.today() + datetime.timedelta(days=30)).isoformat(),
+            'personal_statement': 'My statement here.',
+        })
+        self.assertGreater(Application.objects.count(), count_before)
+
+    def test_list_shows_only_own_applications(self):
+        user_a = self.user
+        user_b = make_user(username='userB')
+        Application.objects.create(user=user_a, university=
